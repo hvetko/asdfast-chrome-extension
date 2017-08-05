2ba8d709aec81947394ad2ccfd1887f0599a57cb
Dejan Hvetkovic <dejan.hvetkovic@getyourguide.com>
2017-08-05 23:37:59 +0200

    popup init

---
+++ js/popup.js
@@ -0,0 +1,41 @@
+function Popup() {
+
+	this.loadASDFast = function () {
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", "http://asdfast.beobit.net/api", true);
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState === XMLHttpRequest.DONE) {
+				var response = JSON.parse(xhr.responseText);
+				$('#paragraph').val(response.text);
+				$('#sentence').val(response.text.split('.').shift());
+			}
+		};
+		xhr.send();
+	};
+
+	this.showTextCopiedNotification = function() {
+		$('#text-copied').show();
+
+		setTimeout(function() {
+			$('#text-copied').fadeOut('fast');
+		}, 1000);
+	}
+}
+
+var popup = new Popup();
+document.addEventListener('DOMContentLoaded', popup.loadASDFast());
+
+$('#reload').click(function () {
+	popup.loadASDFast()
+});
+
+$('#copy-sentence').click(function () {
+	$('#sentence').select();
+	document.execCommand('copy');
+	popup.showTextCopiedNotification();
+});
+$('#copy-paragraph').click(function () {
+	$('#paragraph').select();
+	document.execCommand('copy');
+	popup.showTextCopiedNotification();
+});
